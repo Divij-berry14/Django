342e357d6f27b15515690e5a5a488771d0b2eca2
Divij Berry <divij.berry1999@gmail.com>
2022-04-18 19:40:46 +0530

    Redirect

--- django_basic/views.py
+++ django_basic/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic.base import RedirectView
-
+from django.urls import reverse
 
 def index(request):
     return HttpResponse("<h1>Data Flair Django</h1>Hello, you just configured your First URL")
 
 
 def data_flair(request):
+    # return HttpResponseRedirect("/dataflair")
     return redirect('/dataflair')
 
 
 class Tutorial(RedirectView):
     url = 'https://data-flair.training/blogs/category/django/'
+
